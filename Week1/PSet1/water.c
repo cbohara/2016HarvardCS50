06f34f083662f981e6487911e9029d5f67b67d22
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-10-26 08:48:06 -0700

    completed water

--- Week1/PSet1/water.c
+++ Week1/PSet1/water.c
@@ -3,13 +3,8 @@
 
 int main(void)
 {
-  int min;
   printf("Minutes: ");
-  do
-  {
-    min = get_int();
-  }
-  while (min != sizeof(int));
-  int bottle = (192/16);
-  printf("%i", bottle);
+  int minutes = get_int();
+  int bottles = minutes*(192/16);
+  printf("Your %i minute shower used the equivalent of %i bottles of water!\n", minutes, bottles);
 }

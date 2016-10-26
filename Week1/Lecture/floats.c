8bf936aec9a49c637f79fafb80679597326b8f90
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-10-26 08:43:01 -0700

    week1

--- Week1/Lecture/floats.c
+++ Week1/Lecture/floats.c
@@ -4,10 +4,10 @@
 int main(void)
 {
   printf("x is ");
-  float x = get_int();
+  float x = get_float();
 
   printf("y is ");
-  float y = get_int();
+  float y = get_float();
 
   printf("%f plus %f is %f\n", x, y, x + y);
   printf("%f minus %f is %f\n", x, y, x - y);
--- Week1/PSet1/water.c
+++ Week1/PSet1/water.c
@@ -3,5 +3,13 @@
 
 int main(void)
 {
-  
+  int min;
+  printf("Minutes: ");
+  do
+  {
+    min = get_int();
+  }
+  while (min != sizeof(int));
+  int bottle = (192/16);
+  printf("%i", bottle);
 }

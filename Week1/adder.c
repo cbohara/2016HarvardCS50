30a99664783a7d4fcc7e52a747601f9eb651637f
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-10-25 19:16:38 -0700

    exploring data types

---
+++ Week1/adder.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+  printf("x is ");
+  int x = get_int();
+
+  printf("y is ");
+  int y = get_int();
+
+  printf("sum of x and y is %i\n", x + y);
+}

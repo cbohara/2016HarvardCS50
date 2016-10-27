b5e79ce8606dd10cf3bf6469f710897f544d4b54
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-10-27 14:24:08 -0700

    machine learning lecture

---
+++ Week7/supervised_learning.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plot
+
+# define numpy array of 2-dimensional points for training data
+x_train = np.array([[1,1], [2,2.5], [3,1.2], [5.5,6.3], [6,9], [7,6]])
+y_train = ['red', 'red', 'red', 'blue', 'blue', 'blue']
+
+# create plot with training data
+plot.figure()
+plot.scatter(x_train[:,0], x_train[:,1], s = 170, color = y_train[:])
+plot.show()
+
+# create test point
+x_test = np.array([3,4])
+
+# add test point
+plot.figure()
+plot.scatter(x_train[:,0], x_train[:,1], s = 170, color = y_train[:])
+plot.scatter(x_test[0], x_test[1], s = 170, color = 'green')
+plot.show()
+
+# calculate Euclidean distance (straight-line distance between 2 points)
+def dist(x,y):
+    return np.sqrt(np.sum((x - y)**2))

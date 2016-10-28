56d8ccbfc674fa0d4cbf996b1ad0efd2ac202742
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-10-28 06:51:40 -0700

    building unsupervised algo

--- Week7/text_clustering.py
+++ Week7/text_clustering.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 """
 Unsupervised learning example
@@ -20,16 +21,3 @@ sentences = ['I love CS50. Staff is awesome, awesome, awesome!',
             'I have a dog and a cat.',
             'Best of CS50? Staff. And cakes. Ok, CS50 staff.',
             'My dog keeps chasing my cat. Dogs!']
-
-# create the bag-of-words matrix
-count_vector = Count_Vectorizer(stop_words= 'english')
-x = vectorizer.fit_transform(sentences)
-x.todense()
-
-# define the number of clusters
-k = 2
-# define the proper notation of distance to deal with the sentences
-dist = 1 - cosine_similarity(x)
-# run the algorithm KMeans
-model = KMeans(n_clusters = k)
-model.fit(x)
